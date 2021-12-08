97c2af64d6a17a6056bc1534a0495a3cde556399
Stober <CHRISTIAN_STOBER@dpsk12.org>
2021-12-08 08:45:20 -0700

    Condensed alternate Day 8 solution to a list of rules; not sure if it's actually less readable

--- p8b.py
+++ p8b.py
@@ -23,73 +23,34 @@ def process(line):
     rhs = line[10:] # right hand side 
     assignments = [None] * 10 # assignments from digits to segment sets
     
-    # an entry with just two segments must be the one
-    for i in lhs:
-        if len(i) == 2:
-            assignments[1] = i
-            lhs.remove(i)
-            break
-    
-    # an entry with exactly three segments must be the seven
-    for i in lhs:
-        if len(i) == 3:
-            assignments[7] = i
-            lhs.remove(i)
-            break
-    
-    # an entry with exactly four segments must be the four
-    for i in lhs:
-        if len(i) == 4:
-            assignments[4] = i
-            lhs.remove(i)
-            break
-    
-    # an entry with all seven segments must be the eight
-    for i in lhs:
-        if len(i) == 7:
-            assignments[8] = i
-            lhs.remove(i)
-            break
-    
+    # sequential rules for assigning sets to numerals
+    rules = []
+    # an entry with just two segments must be the one; similar rules for 7, 4, 8
+    rules.append((lambda i: len(i) == 2, 1))
+    rules.append((lambda i: len(i) == 3, 7))
+    rules.append((lambda i: len(i) == 4, 4))
+    rules.append((lambda i: len(i) == 7, 8))
     # a five-segment character containing one must be three
-    for i in lhs:
-        if len(i) == 5 and all([j in i for j in assignments[1]]):
-            assignments[3] = i
-            lhs.remove(i)
-            break
-        
+    rules.append((lambda i: len(i) == 5 and all([j in i for j in assignments[1]]), 3))
     # a remaining five-segment character overlapping three semgents with 
     # four must be five
-    for i in lhs:
-        if len(i) == 5 and len(i.intersection(assignments[4])) == 3:
-            assignments[5] = i
-            lhs.remove(i)
-            break
-      
+    rules.append((lambda i: len(i) == 5 and len(i.intersection(assignments[4])) == 3, 5))     
     # the last remaining five-segment character must be two
-    for i in lhs:
-        if len(i) == 5:
-            assignments[2] = i
-            lhs.remove(i)
-            break
-     
+    rules.append((lambda i: len(i) == 5, 2))     
     # the (six-segment) character containing four must be nine
-    for i in lhs:
-        if all([j in i for j in assignments[4]]):
-            assignments[9] = i
-            lhs.remove(i)
-            break
-
+    rules.append((lambda i: all([j in i for j in assignments[4]]), 9))     
     # the remaining (six-segment) character containing one must be zero
-    for i in lhs:
-        if all([j in i for j in assignments[1]]):
-            assignments[0] = i
-            lhs.remove(i)
-            break
-    
+    rules.append((lambda i: all([j in i for j in assignments[1]]), 0))     
     # the last remaining (six-segment) character must be six
-    assignments[6] = lhs[0]
-    
+    rules.append((lambda i: True, 6))
+
+    for rule, value in rules:
+        for i in lhs:
+            if rule(i):
+                assignments[value] = i
+                lhs.remove(i)
+                break
+
     # now translate the rhs and return
     return int(''.join([str(assignments.index(i)) for i in rhs]))
 
@@ -97,6 +58,6 @@ def process(line):
 outvals = [process(line) for line in data]
 print("Part 2:", sum(outvals))
     
-#pdb.set_trace()
+pdb.set_trace()
 
     
