47054f39f451c9a30b79169fdc99c9274cec9920
Stober <CHRISTIAN_STOBER@dpsk12.org>
2021-12-07 22:44:18 -0700

    Resolved unlikely corner case in Day 7

--- p7.py
+++ p7.py
@@ -7,11 +7,10 @@ positions = [int(i) for i in open(FNAME).read().split(',')]
 p0 = min(positions)
 p1 = max(positions)
 
-scores = [sum([abs(anchor - x) for x in positions]) for anchor in range(p0, p1)]
+scores = [sum([abs(anchor - x) for x in positions]) for anchor in range(p0, p1 + 1)]
 print("Part 1:", min(scores))
 
-scores = [sum([abs(anchor - x) * (abs(anchor - x) + 1) // 2 for x in positions]) for anchor in range(p0, p1)]
+scores = [sum([abs(anchor - x) * (abs(anchor - x) + 1) // 2 for x in positions]) for anchor in range(p0, p1 + 1)]
 print("Part 2:", min(scores))
 
 #pdb.set_trace()
-    
--- template.py
+++ template.py
@@ -1,16 +1,17 @@
 import pdb
 from collections import defaultdict
 
-fname = "in1.txt"
+FNAME = "in1.txt"
 
 #import maputil
-#map = maputil.load(fname)
+#map = maputil.load(FNAME)
 
 def parse_line(line):
     return int(line)
     
-data = [parse_line(i) for i in open(fname).readlines()]
-#data = open(fname).read().split('\n\n')
+data = [parse_line(i) for i in open(FNAME).readlines()]
+#data = [int(i) for i in open(FNAME).read().split(',')]
+#data = open(FNAME).read().split('\n\n')
 
 
 
